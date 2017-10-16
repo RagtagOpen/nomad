9714a51839e896e377ecdc8883338c721b41641d
Joshua Carp <jm.carp@gmail.com>
2017-10-16 00:26:16 -0400

    Add initial form tests.

---
+++ tests/test_forms.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""Test forms."""
+
+from werkzeug.datastructures import MultiDict
+
+from app.auth.forms import ProfileForm
+
+
+class TestProfileForm:
+    """Profile form."""
+    def test_validate_name_missing(self, person):
+        form = ProfileForm(
+            formdata=MultiDict({
+                'preferred_contact': 'email',
+                'gender': 'Female',
+            }),
+            meta={'csrf': False},
+        )
+        assert form.validate() is False
+
+    def test_validate_gender_missing(self, person):
+        form = ProfileForm(
+            formdata=MultiDict({
+                'name': 'foo',
+                'preferred_contact': 'email',
+            }),
+            meta={'csrf': False},
+        )
+        assert form.validate() is False
+
+    def test_validate_gender_invalid(self, person):
+        form = ProfileForm(
+            formdata=MultiDict({
+                'name': 'foo',
+                'preferred_contact': 'email',
+                'gender': 'notachoice',
+            }),
+            meta={'csrf': False},
+        )
+        assert form.validate() is False
+
+    def test_validate_contact_phone_no_number(self, person):
+        form = ProfileForm(
+            formdata=MultiDict({
+                'name': 'foo',
+                'preferred_contact': 'phone',
+                'gender': 'self-describe',
+            }),
+            meta={'csrf': False},
+        )
+        assert form.validate() is False
+        assert len(form.errors['phone_number']) == 1
+        assert "You must enter a phone number" in form.errors['phone_number'][0]
+
+    def test_validate_gender_self_describe_no_description(self, person):
+        form = ProfileForm(
+            formdata=MultiDict({
+                'name': 'foo',
+                'preferred_contact': 'email',
+                'gender': 'self-describe',
+            }),
+            meta={'csrf': False},
+        )
+        assert form.validate() is False
+        assert len(form.errors['gender']) == 1
+        assert "You selected self-describe but didn't self-describe" == form.errors['gender'][0]
+
+    def test_validate_success(self, person):
+        form = ProfileForm(
+            formdata=MultiDict({
+                'name': 'foo',
+                'preferred_contact': 'email',
+                'gender': 'self-describe',
+                'gender_self_describe': 'my-description',
+            }),
+            meta={'csrf': False},
+        )
+        assert form.validate() is True
