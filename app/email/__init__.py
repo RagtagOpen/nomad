4e880ddf65eaddcfc2ff5b726b69779ec8a94d26
Ian Dees <ian.dees@gmail.com>
2017-10-17 21:02:20 -0500

    Serialize uuid for UUID-able objects, deserialize the object on the other side

--- app/email/__init__.py
+++ app/email/__init__.py
@@ -1,24 +1,50 @@
 from flask import current_app, render_template
 from flask_mail import Message
 
+from ..models import UuidMixin
 from .. import mail, rq
 
 
 def send_email(template, recipient, subject, **kwargs):
+    # Convert database model instances to serializable dicts
+    new_kwargs = {}
+    for k, v in kwargs.items():
+        if isinstance(v, UuidMixin):
+            new_kwargs[k] = {
+                'clz': type(v).__name__,
+                'uuid': v.uuid,
+            }
+        else:
+            new_kwargs[k] = v
+
     if current_app.config.get('RQ_ENABLED'):
         # Enqueue the message to send by the RQ worker
-        send_email_queued.queue(template, recipient, subject, **kwargs)
+        send_email_queued.queue(template, recipient, subject, **new_kwargs)
     else:
         # Do the work during the request
-        send_email_queued(template, recipient, subject, **kwargs)
+        send_email_queued(template, recipient, subject, **new_kwargs)
 
 
 @rq.job
 def send_email_queued(template, recipient, subject, **kwargs):
+    import app.models
+
+    new_kwargs = {}
+    for k, v in kwargs.items():
+        if isinstance(v, dict):
+            clz = v.get('clz')
+            uuid = v.get('uuid')
+
+            if clz and uuid:
+                clz = getattr(app.models, clz)
+                new_kwargs[k] = clz.first_by_uuid(uuid)
+        else:
+            new_kwargs[k] = v
+
     message = Message(
         recipients=[recipient],
-        body=render_template('email/{}.txt'.format(template), **kwargs),
-        html=render_template('email/{}.html'.format(template), **kwargs),
+        body=render_template('email/{}.txt'.format(template), **new_kwargs),
+        html=render_template('email/{}.html'.format(template), **new_kwargs),
         subject=subject
     )
 
