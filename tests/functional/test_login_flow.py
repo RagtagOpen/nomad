17042fe1cd0d6cf5f827b36191aa47b4dadad7e2
Ian Dees <ian.dees@gmail.com>
2018-08-20 10:06:51 -0500

    Add test for duplicate email logins

--- tests/functional/test_login_flow.py
+++ tests/functional/test_login_flow.py
@@ -1,4 +1,5 @@
-from . import login_person
+from . import login, login_person
+from ..factories import PersonFactory
 from http import HTTPStatus
 import urllib
 
@@ -12,3 +13,22 @@ class TestLoginFlow:
         assert res.status_code == HTTPStatus.FOUND
         url = urllib.parse.urlparse(res.headers['Location'])
         assert url.path == cancel_carpool_url
+
+    def test_dupe_email_login(self, testapp, db, person, carpool):
+        # Steve logs in once using his Facebook account
+        res = login(testapp, 'steve@facebook', 'stevejobs', 'steve@example.com')
+        assert res.status_code == HTTPStatus.FOUND
+        url = urllib.parse.urlparse(res.headers['Location'])
+        assert url.path == '/profile'
+
+        # ... then logs out
+        res = testapp.post('/logout')
+        assert res.status_code == HTTPStatus.FOUND
+        url = urllib.parse.urlparse(res.headers['Location'])
+        assert url.path == '/'
+
+        # Steve tries to login again, this time using Google
+        res = login(testapp, 'steve@google', 'stevejobs', 'steve@example.com')
+        assert res.status_code == HTTPStatus.FOUND
+        url = urllib.parse.urlparse(res.headers['Location'])
+        assert url.path == '/'
